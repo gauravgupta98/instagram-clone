29342644553692d5b104dfafb1dbd3c62a783d0a
Gaurav Gupta <g1561998@gmail.com>
2021-03-25 09:37:02 +0530

    display comments on the post

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import { formatDistance } from "date-fns";
+import { Link } from "react-router-dom";
 
-export default function Comments({ docId, comments, posted, commentInput }) {
-  return <p> comment </p>;
+export default function Comments({
+  docId,
+  comments: allComments,
+  posted,
+  commentInput,
+}) {
+  const [comments, setComments] = useState(allComments);
+  console.log("fuck", comments);
+  return (
+    <>
+      <div className="p-4 pt-1 pb-4">
+        {comments.length >= 3 && (
+          <p className="text-sm text-gray-base mb-1 cursor-pointer">
+            View all comments
+          </p>
+        )}
+        {comments.slice(0, 3).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
+        <p className="text-gray-base uppercase text-xs mt-2">
+          {formatDistance(posted, new Date())} ago
+        </p>
+      </div>
+    </>
+  );
 }
 
 Comments.propTypes = {
