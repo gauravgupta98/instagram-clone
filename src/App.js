97be761730466ba2232947f5afd7dc0b00ad19a1
Gaurav Gupta <g1561998@gmail.com>
2021-03-26 09:27:54 +0530

    added loggedinroute to ensure user redirects to dashboard

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import UserContext from "./context/user";
 import useAuthListener from "./hooks/use-auth-listener";
+
 import ProtectedRoute from "./helpers/protectedRoute";
+import IsUserLoggedIn from "./helpers/userLoggedInRoute";
 
 const Dashboard = lazy(() => import("./pages/dashboard"));
+const Profie = lazy(() => import("./pages/profile"));
 const NotFound = lazy(() => import("./pages/notfound"));
 const Signup = lazy(() => import("./pages/signup"));
 const Login = lazy(() => import("./pages/login"));
@@ -19,11 +22,24 @@ export default function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGNUP} component={Signup} />
+            <IsUserLoggedIn
+              user={user}
+              loggedInPath={ROUTES.DASHBOARD}
+              path={ROUTES.LOGIN}
+            >
+              <Login />
+            </IsUserLoggedIn>
+            <IsUserLoggedIn
+              user={user}
+              loggedInPath={ROUTES.DASHBOARD}
+              path={ROUTES.SIGNUP}
+            >
+              <Signup />
+            </IsUserLoggedIn>
             <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
               <Dashboard />
             </ProtectedRoute>
+            <Route path={ROUTES.PROFILE} component={Profie} />
             <Route component={NotFound} />
           </Switch>
         </Suspense>
